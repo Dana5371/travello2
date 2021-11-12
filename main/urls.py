3e4b7a4ca0d7cb8ffcedd517c1fe020f8127f7fd
Aliyaversion1 <aliyanasyrbekova@gmail.com>
2021-11-12 17:33:16 +0600

    Aliya, search

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from main.views import CategoryDetailView, HomePageView
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category'),
+    # path('q', )
     
 ]
