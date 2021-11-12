e86ddd2fcc0ca3f54f1e3667ddc97e751572087f
Dana5371 <danaker.qw@gmail.com>
2021-11-12 23:03:10 +0600

    dana post-lists

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import CategoryDetailView, HomePageView
+from main.views import CategoryDetailView, HomePageView, add_post, post_detail
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category'),
-    
+    path('post-detail/<int:pk>/', post_detail, name='post-detail'),
+    path('add-post/', add_post, name='add-post')
 ]
